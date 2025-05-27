xcp-ng/win-xeniface

commit c649edc84f8546b6acb939d8ba2dee0628cf2506

Initial commit of fully open xeniface code
Signed-off-by: Ben Chalmers <Ben.Chalmers@citrix.com>

src/xeniface/ioctls.c
@@ -30,9 +30,10 @@
  */
 
 
-#include "xeniface.h"
+#include "driver.h"
 #include "ioctls.h"
 #include "..\..\include\xeniface_ioctls.h"
+#include "log.h"
 
 static FORCEINLINE BOOLEAN
 __IsValidStr(
@@ -83,7 +84,7 @@ __DisplayMultiSz(
 
 static DECLSPEC_NOINLINE NTSTATUS
 IoctlRead(
-    __in  PFDO_DATA         Fdo,
+    __in  PXENIFACE_FDO         Fdo,
     __in  PCHAR             Buffer,
     __in  ULONG             InLen,
     __in  ULONG             OutLen,
@@ -143,7 +144,7 @@ fail1:
 
 static DECLSPEC_NOINLINE NTSTATUS
 IoctlWrite(
-    __in  PFDO_DATA         Fdo,
+    __in  PXENIFACE_FDO         Fdo,
     __in  PCHAR             Buffer,
     __in  ULONG             InLen,
     __in  ULONG             OutLen
@@ -187,7 +188,7 @@ fail1:
 
 static DECLSPEC_NOINLINE NTSTATUS
 IoctlDirectory(
-    __in  PFDO_DATA         Fdo,
+    __in  PXENIFACE_FDO         Fdo,
     __in  PCHAR             Buffer,
     __in  ULONG             InLen,
     __in  ULONG             OutLen,
@@ -252,7 +253,7 @@ fail1:
 
 static DECLSPEC_NOINLINE NTSTATUS
 IoctlRemove(
-    __in  PFDO_DATA         Fdo,
+    __in  PXENIFACE_FDO         Fdo,
     __in  PCHAR             Buffer,
     __in  ULONG             InLen,
     __in  ULONG             OutLen
@@ -286,7 +287,7 @@ fail1:
 
 NTSTATUS
 XenIFaceIoctl(
-    __in  PFDO_DATA         Fdo,
+    __in  PXENIFACE_FDO         Fdo,
     __in  PIRP              Irp
     )
 {
@@ -326,6 +327,11 @@ XenIFaceIoctl(
     }
 
 done:
+
+	Irp->IoStatus.Status = status;
+
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
     return status;
 }
 

proj/xeniface/xeniface.vcxproj
@@ -129,7 +129,6 @@
   </PropertyGroup>
   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
   <ImportGroup Label="ExtensionSettings">
-    <Import Project="$(VCTargetsPath)\BuildCustomizations\masm.props" />
   </ImportGroup>
   <ImportGroup Label="PropertySheets">
     <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
@@ -183,18 +182,19 @@
   </ItemDefinitionGroup>
   <ItemGroup>
     <FilesToPackage Include="$(TargetPath)" />
-    <FilesToPackage Include="$(OutDir)$(TargetName).pdb" />  
+    <FilesToPackage Include="$(OutDir)$(TargetName).pdb" />
     <FilesToPackage Include="@(Inf->'%(CopyOutput)')" Condition="'@(Inf)'!=''" />
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="../../src/xeniface/ioctls.c" />
-    <ClCompile Include="../../src/xeniface/power.c" />
     <ClCompile Include="../../src/xeniface/wmi.c" />
-    <ClCompile Include="../../src/xeniface/xeniface.c" />
+    <ClCompile Include="..\..\src\xeniface\driver.c" />
+    <ClCompile Include="..\..\src\xeniface\fdo.c" />
+    <ClCompile Include="..\..\src\xeniface\thread.c" />
   </ItemGroup>
   <ItemGroup>
     <Mofcomp Include="../../src/xeniface/wmi.mof">
-        <CreateBinaryMofFile>$(IntDir)/wmi.bmf</CreateBinaryMofFile>
+      <CreateBinaryMofFile>$(IntDir)/wmi.bmf</CreateBinaryMofFile>
     </Mofcomp>
   </ItemGroup>
   <ItemGroup>
@@ -211,8 +211,20 @@
   <ItemGroup>
     <None Include="..\package\package.vcxproj" />
   </ItemGroup>
+  <ItemGroup>
+    <ClInclude Include="..\..\include\util.h" />
+    <ClInclude Include="..\..\src\xeniface\assert.h" />
+    <ClInclude Include="..\..\src\xeniface\driver.h" />
+    <ClInclude Include="..\..\src\xeniface\fdo.h" />
+    <ClInclude Include="..\..\src\xeniface\ioctls.h" />
+    <ClInclude Include="..\..\src\xeniface\log.h" />
+    <ClInclude Include="..\..\src\xeniface\mutex.h" />
+    <ClInclude Include="..\..\src\xeniface\names.h" />
+    <ClInclude Include="..\..\src\xeniface\thread.h" />
+    <ClInclude Include="..\..\src\xeniface\types.h" />
+    <ClInclude Include="..\..\src\xeniface\wmi.h" />
+  </ItemGroup>
   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
   <ImportGroup Label="ExtensionTargets">
-    <Import Project="$(VCTargetsPath)\BuildCustomizations\masm.targets" />
   </ImportGroup>
 </Project>

MAINTAINERS
@@ -1,0 +1,40 @@
+List of maintainers and how to submit changes
+=============================================
+
+This is an early public release of the XenServer XenIface driver.
+
+At the moment we are working hard on improving this early release, to make
+it easier to build and install and test on your own systems.  As such things 
+are in a state of flux, and we may well find it difficult to take submissions.
+
+We hope things will have improved by January 2014
+
+If you do wish to submit code, we recommend reaching out to the maintainers
+first, who will attempt to steer you in the right direction.
+
+We also request you follow these guidelines:
+
+1.  All submissions must be made under the terms of the "Developer's Certificate
+    of Origin" (DC) and should include a Signed-off-by: line.
+
+2.  All Submissions should use Unix line endings for consitency with the rest of
+    the XenServer project
+
+3.  Each patch should include a descriptive commit comment that helps understand
+    why the patch is necessary and why it works. This will be used both for
+    initial review and for new people to understand how the code works later
+
+4.  Make sure you have the right to submit any changes you make. If you do 
+    changes at work you may find your employer owns the patches instead of 
+    you.
+
+
+Maintainers List
+----------------
+
+* Ben Chalmers <ben.chalmers@citrix.com>
+
+* Paul Durrant <paul.durrant@citrix.com>
+
+* Owen Smith <owen.smith@citrix.com>
+

build.py
@@ -265,7 +265,7 @@ def archive(filename, files, tgz=False):
 
 if __name__ == '__main__':
     os.environ['MAJOR_VERSION'] = '7'
-    os.environ['MINOR_VERSION'] = '0'
+    os.environ['MINOR_VERSION'] = '1'
     os.environ['MICRO_VERSION'] = '0'
 
     if 'BUILD_NUMBER' not in os.environ.keys():
@@ -290,6 +290,6 @@ if __name__ == '__main__':
 
 
 
-    listfile = callfnout(['hg','manifest'])
+    listfile = callfnout(['git','ls-tree', '-r', '--name-only', 'HEAD'])
     archive('xeniface\\source.tgz', listfile.splitlines(), tgz=True)
     archive('xeniface.tar', ['xeniface', 'revision'])

include/util.h
@@ -1,0 +1,279 @@
+/* Copyright (c) Citrix Systems Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, 
+ * with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ * *   Redistributions of source code must retain the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer.
+ * *   Redistributions in binary form must reproduce the above 
+ *     copyright notice, this list of conditions and the 
+ *     following disclaimer in the documentation and/or other 
+ *     materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#include <ntddk.h>
+
+#include "assert.h"
+
+#define	P2ROUNDUP(_x, _a)   \
+        (-(-(_x) & -(_a)))
+
+static FORCEINLINE LONG
+__ffs(
+    IN  unsigned long long  mask
+    )
+{
+    unsigned char           *array = (unsigned char *)&mask;
+    unsigned int            byte;
+    unsigned int            bit;
+    unsigned char           val;
+
+    val = 0;
+
+    byte = 0;
+    while (byte < 8) {
+        val = array[byte];
+
+        if (val != 0)
+            break;
+
+        byte++;
+    }
+    if (byte == 8)
+        return -1;
+
+    bit = 0;
+    while (bit < 8) {
+        if (val & 0x01)
+            break;
+
+        val >>= 1;
+        bit++;
+    }
+
+    return (byte * 8) + bit;
+}
+
+#define __ffu(_mask)  \
+        __ffs(~(_mask))
+
+static FORCEINLINE LONG
+__InterlockedAdd(
+    IN  LONG    *Value,
+    IN  LONG    Delta
+    )
+{
+    LONG        New;
+    LONG        Old;
+
+    do {
+        Old = *Value;
+        New = Old + Delta;
+    } while (InterlockedCompareExchange(Value, New, Old) != Old);
+
+    return New;
+}
+
+static FORCEINLINE LONG
+__InterlockedSubtract(
+    IN  LONG    *Value,
+    IN  LONG    Delta
+    )
+{
+    LONG        New;
+    LONG        Old;
+
+    do {
+        Old = *Value;
+        New = Old - Delta;
+    } while (InterlockedCompareExchange(Value, New, Old) != Old);
+
+    return New;
+}
+
+typedef struct _NON_PAGED_BUFFER_HEADER {
+    SIZE_T  Length;
+    ULONG   Tag;
+} NON_PAGED_BUFFER_HEADER, *PNON_PAGED_BUFFER_HEADER;
+
+typedef struct _NON_PAGED_BUFFER_TRAILER {
+    ULONG   Tag;
+} NON_PAGED_BUFFER_TRAILER, *PNON_PAGED_BUFFER_TRAILER;
+
+static FORCEINLINE PVOID
+__AllocateNonPagedPoolWithTag(
+    IN  SIZE_T                  Length,
+    IN  ULONG                   Tag
+    )
+{
+    PUCHAR                      Buffer;
+    PNON_PAGED_BUFFER_HEADER    Header;
+    PNON_PAGED_BUFFER_TRAILER   Trailer;
+
+    ASSERT(Length != 0);
+
+    Buffer = (PUCHAR)ExAllocatePoolWithTag(NonPagedPool,
+                                   sizeof (NON_PAGED_BUFFER_HEADER) +
+                                   Length +
+                                   sizeof (NON_PAGED_BUFFER_TRAILER),
+                                   Tag);
+    if (Buffer == NULL)
+        goto done;
+
+    RtlZeroMemory(Buffer, 
+                  sizeof (NON_PAGED_BUFFER_HEADER) +
+                  Length +
+                  sizeof (NON_PAGED_BUFFER_TRAILER));
+
+    Header = (PNON_PAGED_BUFFER_HEADER)Buffer;
+    Header->Length = Length;
+    Header->Tag = Tag;
+
+    Buffer += sizeof (NON_PAGED_BUFFER_HEADER);
+
+    Trailer = (PNON_PAGED_BUFFER_TRAILER)(Buffer + Length);
+    Trailer->Tag = Tag;
+
+done:
+    return Buffer;
+}
+
+static FORCEINLINE VOID
+__FreePoolWithTag(
+    IN  PVOID                   _Buffer,
+    IN  ULONG                   Tag
+    )
+{
+    PUCHAR                      Buffer = (PUCHAR)_Buffer;
+    SIZE_T                      Length;
+    PNON_PAGED_BUFFER_HEADER    Header;
+    PNON_PAGED_BUFFER_TRAILER   Trailer;
+
+    ASSERT(Buffer != NULL);
+
+    Buffer -= sizeof (NON_PAGED_BUFFER_HEADER);
+
+    Header = (PNON_PAGED_BUFFER_HEADER)Buffer;
+    ASSERT3U(Tag, ==, Header->Tag);
+    Length = Header->Length;
+
+    Buffer += sizeof (NON_PAGED_BUFFER_HEADER);
+
+    Trailer = (PNON_PAGED_BUFFER_TRAILER)(Buffer + Length);
+    ASSERT3U(Tag, ==, Trailer->Tag);
+
+    Buffer -= sizeof (NON_PAGED_BUFFER_HEADER);
+
+    RtlFillMemory(Buffer, 
+                  sizeof (NON_PAGED_BUFFER_HEADER) +
+                  Length +
+                  sizeof (NON_PAGED_BUFFER_TRAILER),
+                  0xAA);
+
+    ExFreePoolWithTag(Buffer, Tag);
+}
+
+static FORCEINLINE PMDL
+__AllocatePage(
+    VOID
+    )
+{
+    PHYSICAL_ADDRESS    LowAddress;
+    PHYSICAL_ADDRESS    HighAddress;
+    LARGE_INTEGER       SkipBytes;
+    SIZE_T              TotalBytes;
+    PMDL                Mdl;
+    PUCHAR              MdlMappedSystemVa;
+    NTSTATUS            status;
+
+    LowAddress.QuadPart = 0ull;
+    HighAddress.QuadPart = ~0ull;
+    SkipBytes.QuadPart = 0ull;
+    TotalBytes = (SIZE_T)PAGE_SIZE;
+
+    Mdl = MmAllocatePagesForMdlEx(LowAddress,
+                                  HighAddress,
+                                  SkipBytes,
+                                  TotalBytes,
+                                  MmCached,
+                                  0);
+
+    status = STATUS_NO_MEMORY;
+    if (Mdl == NULL)
+        goto fail1;
+
+    ASSERT((Mdl->MdlFlags & (MDL_MAPPED_TO_SYSTEM_VA |
+                             MDL_PARTIAL_HAS_BEEN_MAPPED |
+                             MDL_PARTIAL |
+                             MDL_PARENT_MAPPED_SYSTEM_VA |
+                             MDL_SOURCE_IS_NONPAGED_POOL |
+                             MDL_IO_SPACE)) == 0);
+
+    MdlMappedSystemVa = MmMapLockedPagesSpecifyCache(Mdl,
+                                                     KernelMode,
+						                             MmCached,   
+						                             NULL,
+						                             FALSE,
+						                             NormalPagePriority);
+
+    status = STATUS_UNSUCCESSFUL;
+    if (MdlMappedSystemVa == NULL)
+        goto fail2;
+
+    ASSERT3P(MdlMappedSystemVa, ==, Mdl->MappedSystemVa);
+
+    RtlZeroMemory(MdlMappedSystemVa, PAGE_SIZE);
+
+    return Mdl;
+
+fail2:
+    Error("fail2\n");
+
+    MmFreePagesFromMdl(Mdl);
+    ExFreePool(Mdl);
+
+fail1:
+    Error("fail1 (%08x)\n", status);
+
+    return NULL;
+}
+
+static FORCEINLINE VOID
+__FreePage(
+    IN	PMDL	Mdl
+    )
+{
+    PUCHAR	MdlMappedSystemVa;
+
+    ASSERT(Mdl->MdlFlags & MDL_MAPPED_TO_SYSTEM_VA);
+    MdlMappedSystemVa = Mdl->MappedSystemVa;
+
+    RtlFillMemory(MdlMappedSystemVa, PAGE_SIZE, 0xAA);
+    
+    MmUnmapLockedPages(MdlMappedSystemVa, Mdl);
+
+    MmFreePagesFromMdl(Mdl);
+}
+
+#endif  // _UTIL_H

kdfiles.py
@@ -1,0 +1,26 @@
+#!python -u
+
+import os, sys
+import subprocess
+import glob
+from pprint import pprint
+
+def regenerate_kdfiles(filename, arch, pkg, source):
+	cwd = os.getcwd()
+	file = open(filename, 'w')
+	os.chdir(pkg + '/' + arch)
+	drivers = glob.glob('*.sys')
+	pprint(drivers)
+	for driver in drivers:
+		file.write("map\n")
+		file.write('\SystemRoot\System32\drivers\\' + driver + '\n')
+		file.write(source + '\\' + pkg + '\\' + arch + '\\' + driver + '\n')
+		file.write('\n')
+	os.chdir(cwd)
+	file.close()
+
+if __name__ == '__main__':
+	pkg = 'xeniface'
+	source = os.getcwd()
+	regenerate_kdfiles('kdfiles32.txt', 'x86', pkg, source)
+	regenerate_kdfiles('kdfiles64.txt', 'x64', pkg, source)

proj/package/package.vcxproj.user
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup>
+    <SignMode>ProductionSign</SignMode>
+    <ProductionCertificate>CN="Citrix Systems, Inc.", OU=Digital ID Class 3 - Microsoft Software Validation v2, O="Citrix Systems, Inc.", L=Santa Clara, S=California, C=US | 9505C081954CC0A3E8B904458ACACD72EABD98B5</ProductionCertificate>
+    <TimeStampServer>http://timestamp.verisign.com/scripts/timstamp.dll</TimeStampServer>
+  </PropertyGroup>
+</Project>

commit e1f4f7d52c05b607099d26dae305b114a8e9f0eb

Restrict default access to Xeniface device
Without assigning an explicit SDDL via xeniface.inf, any user can open
the Xeniface FDO via its default security descriptor.

Additionally, validate user read+write access to the Xeniface FDO before
allowing IOCTLs to this interface.

This is part of XSA-468 / CVE-2025-27463.

Fixes: c649edc84f85 ("Initial commit of fully open xeniface code")
Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>
Reviewed-By: Owen Smith <owen.smith@cloud.com>

src/xeniface.inf
@@ -72,6 +72,12 @@ xenagent_@MAJOR_VERSION@_@MINOR_VERSION@_@MICRO_VERSION@_@BUILD_NUMBER@.dll,xena
 CopyFiles=XenIface_CopyFiles
 CopyFiles=XenAgent_CopyFiles
 
+[XenIface_Inst.HW]
+AddReg=XenIface_Inst.HW.AddReg
+
+[XenIface_Inst.HW.AddReg]
+HKR,,Security,,"D:P(A;;GA;;;SY)(A;;GA;;;BA)"  ; SDDL_DEVOBJ_SYS_ALL_ADM_ALL
+
 [Xeniface_Inst.Services]
 AddService = xeniface, 0x0002, XenIface_Service
 AddService = xenagent, 0x0800, XenAgent_Service,XenAgent_EventLog

src/xeniface/ioctls.c
@@ -33,6 +33,7 @@
 
 #include <ntifs.h>
 #include <procgrp.h>
+#include <wdmsec.h>
 #include "driver.h"
 #include "ioctls.h"
 #include "xeniface_ioctls.h"
@@ -251,6 +252,10 @@ XenIfaceIoctl(
 
     status = STATUS_DEVICE_NOT_READY;
     if (Fdo->InterfacesAcquired == FALSE)
+        goto done;
+
+    status = WdmlibIoValidateDeviceIoControlAccess(Irp, FILE_READ_ACCESS | FILE_WRITE_ACCESS);
+    if (status != STATUS_SUCCESS)
         goto done;
 
     switch (ControlCode) {

vs2019/xeniface/xeniface.vcxproj
@@ -31,7 +31,7 @@
       <AdditionalIncludeDirectories>..\..\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
     </ResourceCompile>
     <Link>
-      <AdditionalDependencies>$(DDK_LIB_PATH)\ntstrsafe.lib;$(DDK_LIB_PATH)\procgrp.lib;%(AdditionalDependencies)</AdditionalDependencies>
+      <AdditionalDependencies>$(DDK_LIB_PATH)\ntstrsafe.lib;$(DDK_LIB_PATH)\procgrp.lib;$(DDK_LIB_PATH)\wdmsec.lib;%(AdditionalDependencies)</AdditionalDependencies>
       <AdditionalOptions>/INTEGRITYCHECK %(AdditionalOptions)</AdditionalOptions>
       <LinkTimeCodeGeneration>UseLinkTimeCodeGeneration</LinkTimeCodeGeneration>
       <CETCompat>true</CETCompat>

vs2022/xeniface/xeniface.vcxproj
@@ -31,7 +31,7 @@
       <AdditionalIncludeDirectories>..\..\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
     </ResourceCompile>
     <Link>
-      <AdditionalDependencies>$(DDK_LIB_PATH)\ntstrsafe.lib;$(DDK_LIB_PATH)\procgrp.lib;%(AdditionalDependencies)</AdditionalDependencies>
+      <AdditionalDependencies>$(DDK_LIB_PATH)\ntstrsafe.lib;$(DDK_LIB_PATH)\procgrp.lib;$(DDK_LIB_PATH)\wdmsec.lib;%(AdditionalDependencies)</AdditionalDependencies>
       <AdditionalOptions>/INTEGRITYCHECK %(AdditionalOptions)</AdditionalOptions>
       <LinkTimeCodeGeneration>UseLinkTimeCodeGeneration</LinkTimeCodeGeneration>
       <CETCompat>true</CETCompat>